4e80225be0385c5a939018fbf09baa846b88d4ef
zakiajaffar <38943122+zakiajaffar@users.noreply.github.com>
2018-10-01 16:52:16 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function getWeatherIcon(weatherType) {
     // TODO: try using switch statement here instead of if/else
     if (weatherType == "01d" || weatherType == "01n") {
         icon = "wi-day-sunny";
+
     }
     else if (weatherType == "02d" || weatherType == "02n") {
         icon = "wi-day-cloudy";
@@ -36,12 +37,13 @@ function getWeatherIcon(weatherType) {
 
 
 function getWeather() {
-
+    
     document.querySelector(".weather-info").style.display = "block";
     document.querySelector(".weather-comment").style.display = "none";
-
+    
     const cityName = document.querySelector("input").value;
 
+    
     $.ajax({
         url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=1cafa1aa713dc95fd5cd808d0df22dfc&units=metric`,
         success: function (data) {
@@ -53,45 +55,49 @@ function getWeather() {
             document.querySelector(".description").innerHTML = data.weather[0].main;
             document.querySelector(".min").innerHTML = Math.round(data.main.temp_min);
             document.querySelector(".max").innerHTML = Math.round(data.main.temp_max);
-
+            
             let calcTime = new Date();
             let sunsetTime = new Date(data.sys.sunset * 1000);
             let sunriseTime= new Date(data.sys.sunrise * 1000);
-
+            
             if (sunsetTime > calcTime && sunriseTime < calcTime){
-
-             document.querySelector("body").style["background-color"]='#4db4b2ba';
-              document.querySelector("body").style["color"] = 'white'
-              let list = document.querySelectorAll(".cards div");
-              console.log(list);
-              for(let i = 0; i < list.length; i++){
-                list[i].style.border = "1px solid #1e3178";
-              }
-        
-         } else {
-            document.querySelector("body").style["background-color"]='#000000';
-            document.querySelector("body").style["color"] = 'white';
-            let list = document.querySelectorAll(".cards div");
-              console.log(list);
-              for(let i = 0; i < list.length; i++){
-                list[i].style.border = "1px solid blue";
-              }
+                document.querySelector("body").style["background-image"]=`url(picture.jpg)`;
+                document.querySelector("body").style["height"] =`100%`;
+                document.querySelector("body").style["color"] = 'white'
+                let list = document.querySelectorAll(".cards div");
+                console.log(list);
+                for(let i = 0; i < list.length; i++){
+                    list[i].style.border = "1px solid #1e3178";
+                    
+                }
+                
+            } else {
+                document.querySelector("body").style["background-image"]=`url(cosmo.jpg)`;
+                document.querySelector("body").style["height"] =`100%`;
+                document.querySelector("body").style["color"] = 'white';
+                let list = document.querySelectorAll(".cards div");
+                console.log(list);
+                for(let i = 0; i < list.length; i++){
+                    list[i].style.border = "1px solid white";
+                  
+                }
+                let list2 = document.querySelectorAll(".cloudy, .cloud");
+                console.log(list2);
+                for(let i = 0; i < list2.length; i++){
+                    list2[i].style.color = "white";
+                  
+                }
+               
             }
+        },
+        
+        error: function (err) {
+            console.log(err);
+            alert(err.responseJSON.message);
+            document.querySelector(".weather-info").style.display = "none";
+        }
+    })
 
-
-
-
-
-    },
-
-    error: function (err) {
-        console.log(err);
-        alert(err.responseJSON.message);
-        document.querySelector(".weather-info").style.display = "none";
-    }
-})
-
-    
     $.ajax({
         url: `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=1cafa1aa713dc95fd5cd808d0df22dfc&units=metric`,
         success: function (data) {
@@ -106,21 +112,21 @@ function getWeather() {
                 document.querySelector(`.cloud-${i}`).innerHTML = `<i class="wi ${getWeatherIcon(data.list[index].weather[0].icon)}"></i>`;
             }
         },
-
+        
         error: function (error) {
             console.log(error);
             document.querySelector(".error").innerHTML = (error.responseJSON.message);
             document.querySelector(".weather-info").style.display = "none";
             document.querySelector(".main").style.display = "none";
             document.querySelector(".error").style.display = "block";
-
-
-
-
+            
+            
+            
+            
         }
-
+        
     })
-
+    
 }
 
 function mySubmitFunction() {
